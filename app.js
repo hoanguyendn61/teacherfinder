0db85c66bfc044da8e2742f7b582424df1a4e441
Hoa Nguyen <hoanguyendn61@gmail.com>
2022-11-29 20:46:19 +0700

    init commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var app = express();
+require("dotenv").config();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Teacher = require("./models/teacher");
 var User = require("./models/user");
 var Comment = require("./models/comment");
 var flash = require("connect-flash");
-
 
 var seedDB = require("./seeds");
 var passport = require("passport");
@@ -17,13 +17,11 @@ var commentRoutes = require("./routes/comments");
 var TeacherRoutes = require("./routes/teachers");
 var indexRoutes = require("./routes/index");
 var reviewRoutes = require("./routes/reviews");
-
-
-mongoose.connect(process.env.DATABASEURL);
+mongoose.connect(process.env.DATABASE_URL);
 
 // mongoose.connect("mongodb://localhost/27017");
 
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
@@ -33,22 +31,24 @@ app.set("view engine", "ejs");
 app.use(flash());
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Once again blabla",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/", indexRoutes);
@@ -56,11 +56,9 @@ app.use("/teachers", TeacherRoutes);
 app.use("/teachers/:id/comments", commentRoutes);
 app.use("/teachers/:id/reviews", reviewRoutes);
 
-
 //=============================================================================
 //Starts the server
 
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("The IRMT server has started!");
+app.listen(process.env.PORT, process.env.IP, function () {
+  console.log("The server has started! at ", process.env.PORT);
 });
-
--- models/user.js
+++ models/user.js
@@ -2,20 +2,20 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: { type: String, unique: true },
-    password: String,
-    avatar: String,
-    imageId: String,
-    firstName: String,
-    lastName: String,
-    about: String,
-    email: { type: String, unique: true },
-    resetPasswordToken: String,
-    resetPasswordExpires: Date,
-    isAdmin: {type: Boolean, default: false},
-    isVerified: {type: Boolean, default: false},
-    isVerifiedToken: String,
-    isVerifiedExpires: Date
+  username: { type: String, unique: true },
+  password: String,
+  avatar: String,
+  imageId: String,
+  firstName: String,
+  lastName: String,
+  about: String,
+  email: { type: String, unique: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  isAdmin: { type: Boolean, default: false },
+  isVerified: { type: Boolean, default: false },
+  isVerifiedToken: String,
+  isVerifiedExpires: Date,
 });
 
 UserSchema.plugin(passportLocalMongoose);
